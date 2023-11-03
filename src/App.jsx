8571c0dcee3cd5575d978a52cc9bd74a8da999bd
Jhonny Iriarte <iriarte.condori.jhonny25@gamil.com>
2023-11-03 11:07:59 -0400

    mi primer commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-import Header from './components/header'
+import Header from './components/Header'
 import Body from './components/Body'
 import TestimonialsSlider from './components/TestimonialsSlider'
 import Footer from './components/footer'
